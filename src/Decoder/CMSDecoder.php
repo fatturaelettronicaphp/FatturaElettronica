a57ba799d23338913f12ad9bcbd06dea79f4784e
Daniele Rosario <daniele@weble.it>
2021-11-22 14:37:33 +0100

    php8 decoder

--- src/Decoder/CMSDecoder.php
+++ src/Decoder/CMSDecoder.php
@@ -12,6 +12,14 @@ class CMSDecoder implements DigitalDocumentDecodeInterface
         $xmlPath = tempnam(sys_get_temp_dir(), basename($filePath));
         $pemPath = tempnam(sys_get_temp_dir(), basename($filePath));
 
+        // php 8 only
+        if (
+            function_exists('openssl_cms_verify') &&
+            openssl_cms_verify($filePath, OPENSSL_CMS_NOVERIFY + OPENSSL_CMS_NOSIGS, $pemPath, [__DIR__ . '/ca.pem'], __DIR__ . '/ca.pem', $xmlPath)
+        ) {
+            return (new XMLDecoder())->decode($xmlPath);
+        }
+
         $exitCode = 0;
         $output   = [];
         exec(sprintf('openssl cms -verify -noverify -nosigs -in %s --inform DER  -out %s 2> /dev/null', $pemPath, $xmlPath), $output, $exitCode);
