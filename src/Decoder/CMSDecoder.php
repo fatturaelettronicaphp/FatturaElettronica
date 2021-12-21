dc462024878e0eda9deadeb55973a2df8d89aadf
Daniele Rosario <daniele@weble.it>
2021-12-21 16:56:26 +0100

    CMS Decoder (#34)
    * php8 decoder
    
    * cms tests

--- src/Decoder/CMSDecoder.php
+++ src/Decoder/CMSDecoder.php
@@ -12,8 +12,18 @@ class CMSDecoder implements DigitalDocumentDecodeInterface
         $xmlPath = tempnam(sys_get_temp_dir(), basename($filePath));
         $pemPath = tempnam(sys_get_temp_dir(), basename($filePath));
 
+        $p7mFilePath = $this->convertFromDERtoSMIMEFormat($filePath);
+
+        // php 8 only
+        if (
+            function_exists('openssl_cms_verify') &&
+            openssl_cms_verify($p7mFilePath, OPENSSL_CMS_NOVERIFY + OPENSSL_CMS_NOSIGS, $pemPath, array(__DIR__ . '/ca.pem'), __DIR__ . '/ca.pem', $xmlPath)
+        ) {
+            return (new XMLDecoder())->decode($xmlPath);
+        }
+
         $exitCode = 0;
-        $output   = [];
+        $output = [];
         exec(sprintf('openssl cms -verify -noverify -nosigs -in %s --inform DER  -out %s 2> /dev/null', $pemPath, $xmlPath), $output, $exitCode);
 
         if ($exitCode !== 0) {
@@ -21,5 +31,22 @@ class CMSDecoder implements DigitalDocumentDecodeInterface
         }
 
         return (new XMLDecoder())->decode($xmlPath);
+    }
+
+    protected function convertFromDERtoSMIMEFormat(string $file): string
+    {
+        $pemPath = tempnam(sys_get_temp_dir(), basename($file));
+        $to = <<<TXT
+MIME-Version: 1.0
+Content-Disposition: attachment; filename="smime.p7m"
+Content-Type: application/x-pkcs7-mime; smime-type=signed-data; name="smime.p7m"
+Content-Transfer-Encoding: base64
+\n
+TXT;
+        $from = file_get_contents($file);
+        $to .= chunk_split(base64_encode($from));
+        file_put_contents($pemPath, $to);
+
+        return $pemPath;
     }
 }
