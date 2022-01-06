a1713a79d024bf03c6934c2ffa5e37d51cd8afb8
Kristian Lentino <kristian.lentino@dieffetech.it>
2022-01-06 15:10:53 +0100

    refactoring

--- src/Senders/AbstractSender.php
+++ src/Senders/AbstractSender.php
@@ -8,62 +8,84 @@ use FatturaElettronicaPhp\FatturaElettronica\Enums\SenderEnvironments;
 
 abstract class AbstractSender
 {
-	/**
-	 * The environment where the code is executed:
-	 *  - development -> test environment
-	 *  - production -> real environment
-	 * @var string $environment
-	 */
-	public $environment;
-	/**
-	 * @param DigitalDocument $document
-	 * @return bool
-	 */
-	abstract public function send($document);
-	/**
+    /**
+     * The environment where the code is executed:
+     *  - development -> test environment
+     *  - production -> real environment
+     *
+     * @var string $environment
+     */
+    public $environment;
+    /**
+     * Abstract method to implement the send method
 	 * @return mixed
-	 */
-	abstract protected function login();
+     * @param  DigitalDocument $document the invoice to send
+     */
+    abstract public function send($document);
+    /**
+     * Abstract method to implement the auth method
+     *
+     * @return mixed
+     */
+    abstract protected function login();
 
-	/**
-	 * @return string
-	 */
-	public function getEnvironment()
-	{
-		return $this->environment;
-	}
+    /**
+     * Return the current environment
+     *
+     * @return string
+     */
+    public function getEnvironment()
+    {
+        return $this->environment;
+    }
 
-	/**
-	 * @param SenderEnvironments $environment
-	 */
-	public function setEnvironment($environment)
-	{
-		$this->environment = $environment;
-	}
+    /**
+     * Set the environment
+     *
+     * @param SenderEnvironments $environment the desidered environment
+	 * @return void
+     */
+    public function setEnvironment($environment)
+    {
+        $this->environment = $environment;
+    }
 
-	public function setProduction()
-	{
-		$this->environment = SenderEnvironments::production()->value;
-	}
+    /**
+     * Set the environment of the sender to production
+     *
+     * @return void
+     */
+    public function setProduction()
+    {
+        $this->environment = SenderEnvironments::production()->value;
+    }
+    /**
+     * Set the environment of the sender to development
+     *
+     * @return void
+     */
+    public function setDevelopment()
+    {
+        $this->environment = SenderEnvironments::development()->value;
+    }
 
-	public function setDevelopment()
-	{
-		$this->environment = SenderEnvironments::development()->value;
-	}
+    /**
+     * Check if the environment of the sender to development
+     *
+     * @return bool
+     */
+    public function isDevelopment()
+    {
+        return $this->environment === EnvironmentEnum::ENVIRONMENT_DEVELOPMENT;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isDevelopment()
-	{
-		return $this->environment === EnvironmentEnum::ENVIRONMENT_DEVELOPMENT;
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function isProduction()
-	{
-		return $this->environment === EnvironmentEnum::ENVIRONMENT_PRODUCTION;
-	}
+    /**
+     * Check if the environment of the sender to production
+     *
+     * @return bool
+     */
+    public function isProduction()
+    {
+        return $this->environment === EnvironmentEnum::ENVIRONMENT_PRODUCTION;
+    }
 }
--- src/Senders/ArubaSender.php
+++ src/Senders/ArubaSender.php
@@ -2,117 +2,125 @@
 
 namespace FatturaElettronicaPhp\FatturaElettronica\Senders;
 
+use FatturaElettronicaPhp\FatturaElettronica\Exceptions\Senders\EmptyCredentialsException;
 use JsonException;
-
+/**
+ * Factory class of the service Aruba
+ */
 class ArubaSender extends AbstractSender
 {
-	protected const DEMO_AUTH_URL = 'https://demoauth.fatturazioneelettronica.aruba.it';
-	protected const DEMO_INVOICE_URL = 'https://demows.fatturazioneelettronica.aruba.it';
-	protected const INVOICE_URL = 'https://auth.fatturazioneelettronica.aruba.it';
-	protected const AUTH_URL = 'https://ws.fatturazioneelettronica.aruba.it';
+    protected const DEMO_AUTH_URL = 'https://demoauth.fatturazioneelettronica.aruba.it';
+    protected const DEMO_INVOICE_URL = 'https://demows.fatturazioneelettronica.aruba.it';
+    protected const INVOICE_URL = 'https://auth.fatturazioneelettronica.aruba.it';
+    protected const AUTH_URL = 'https://ws.fatturazioneelettronica.aruba.it';
 
-	/**
-	 * @var string $upload_invoice_url
-	 */
-	private $upload_invoice_url = '';
-	/**
-	 * @var string $auth_url
-	 */
-	private $auth_url = '';
-	/**
-	 * @var string acube username
-	 */
-	private $username;
-	/**
-	 * @var string acube password
-	 */
-	private $password;
+    /**
+     * @var string $upload_invoice_url
+     */
+    private $_upload_invoice_url = '';
+    /**
+     * @var string $auth_url
+     */
+    private $_auth_url = '';
+    /**
+     * @var string aruba username
+     */
+    private $_username;
+    /**
+     * @var string aruba password
+     */
+    private $_password;
 
-	public function __construct($username,$password)
-	{
-		$this->username = $username;
-		$this->password = $password;
-	}
+    public function __construct($username,$password)
+    {
 
-	/**
-	 * @inheritDoc
-	 * @throws JsonException
-	 */
-	public function send($document)
-	{
-		$this->getUrlsByEnvironment();
-		$authToken = $this->login();
-		$url = $this->upload_invoice_url . '/services/invoice/upload';
-		$ch = curl_init($url);
-		$headers = [
-			'Accept: application/json',
-			'Content-Type: application/json',
-			'charset: UTF8-8',
-			"Authorization: Bearer $authToken"
-		];
+        $this->_username = $username;
+        $this->_password = $password;
+    }
 
-		$data = [
-			'dataFile' => base64_encode($document->serialize()->saveXML())
-		];
-		$options = array(
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_HTTPHEADER => $headers,
-			CURLOPT_POSTFIELDS => json_encode($data, JSON_THROW_ON_ERROR)
-		);
-		curl_setopt_array($ch, $options);
-		$result = curl_exec($ch);
-		$result = json_decode($result, true, 512, JSON_THROW_ON_ERROR);
+    /**
+     * @inheritDoc
+     * @throws     JsonException
+     */
+    public function send($document)
+    {
+        $this->getUrlsByEnvironment();
+        $authToken = $this->login();
+        $url = $this->_upload_invoice_url . '/services/invoice/upload';
+        $ch = curl_init($url);
+        $headers = [
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'charset: UTF8-8',
+        "Authorization: Bearer $authToken"
+        ];
 
-		return $result;
-	}
+        $data = [
+        'dataFile' => base64_encode($document->serialize()->saveXML())
+        ];
+        $options = array(
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_POSTFIELDS => json_encode($data, JSON_THROW_ON_ERROR)
+        );
+        curl_setopt_array($ch, $options);
+        $result = curl_exec($ch);
+        $result = json_decode($result, true, 512, JSON_THROW_ON_ERROR);
 
-	/**
-	 * @inheritDoc
-	 *
-	 * @throws JsonException
-	 */
-	protected function login()
-	{
-		$auth_url = $this->auth_url . '/auth/signin';
-		$ch = curl_init($auth_url);
-		$data = [
-			'grant_type' => 'password',
-			'username' => $this->username,
-			'password' => $this->password,
-		];
+        return $result;
+    }
 
-		curl_setopt($ch, CURLOPT_POST, 1);
-		$options = array(
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_HTTPHEADER => array(
-				'Content-type: application/x-www-form-urlencoded',
-			),
-			CURLOPT_POSTFIELDS => http_build_query($data)
-		);
-
-		curl_setopt_array($ch, $options);
-
-		$result = curl_exec($ch);
-
-		$result = json_decode($result, true, 512, JSON_THROW_ON_ERROR);
-
-		return $result['access_token']??false;
-	}
-
-	/**
-	 * @return void
-	 */
-	private function getUrlsByEnvironment()
-	{
-		switch ($this->environment){
-			case $this->isDevelopment():
-				$this->auth_url = self::DEMO_AUTH_URL;
-				$this->upload_invoice_url = self::DEMO_INVOICE_URL;
-				break;
-			case $this->isProduction():
-				$this->auth_url = self::AUTH_URL;
-				$this->upload_invoice_url = self::INVOICE_URL;
-				break;
+    /**
+     * @inheritDoc
+     *
+     * @throws JsonException
+     */
+    protected function login()
+    {
+		if(empty($this->_password) || empty($this->_username)) {
+			throw new EmptyCredentialsException("Credentials not provided");
 		}
-	}
+
+        $auth_url = $this->_auth_url . '/auth/signin';
+        $ch = curl_init($auth_url);
+        $data = [
+        'grant_type' => 'password',
+        'username' => $this->_username,
+        'password' => $this->_password,
+        ];
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $options = array(
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => array(
+        'Content-type: application/x-www-form-urlencoded',
+        ),
+        CURLOPT_POSTFIELDS => http_build_query($data)
+        );
+
+        curl_setopt_array($ch, $options);
+
+        $result = curl_exec($ch);
+
+        $result = json_decode($result, true, 512, JSON_THROW_ON_ERROR);
+
+        return $result['access_token']??false;
+    }
+
+    /**
+     * @return void
+     */
+    private function getUrlsByEnvironment()
+    {
+        switch ($this->environment){
+        case $this->isDevelopment():
+            $this->_auth_url = self::DEMO_AUTH_URL;
+            $this->_upload_invoice_url = self::DEMO_INVOICE_URL;
+            break;
+        case $this->isProduction():
+            $this->_auth_url = self::AUTH_URL;
+            $this->_upload_invoice_url = self::INVOICE_URL;
+            break;
+        }
+    }
 }
