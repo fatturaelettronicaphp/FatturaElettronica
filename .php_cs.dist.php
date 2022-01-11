bc922c4c216ea9b194be5abd32b22f83612d39dd
Daniele Rosario <daniele@weble.it>
2022-01-11 11:50:01 +0100

    fix php cs fixer (lol)

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -13,19 +13,15 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR2'                             => true,
-        'array_indentation'                 => true,
+        '@PSR12'                            => true,
         'array_syntax'                      => ['syntax' => 'short'],
-        'no_extra_consecutive_blank_lines'  => true,
-        'ordered_imports'                   => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports'                   => ['sort_algorithm' => 'alpha'],
         'no_unused_imports'                 => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline'       => true,
         'phpdoc_scalar'                     => true,
         'unary_operator_spaces'             => true,
-        'binary_operator_spaces'            => [
-            'default' => 'align',
-        ],
+        'binary_operator_spaces'            => true,
         'blank_line_before_statement'       => [
             'statements' => [
                 'break',
@@ -33,16 +29,20 @@ return (new PhpCsFixer\Config())
                 'declare',
                 'return',
                 'throw',
-                'try',
+                'try'
             ],
         ],
         'phpdoc_single_line_var_spacing'    => true,
         'phpdoc_var_without_name'           => true,
+        'class_attributes_separation'       => [
+            'elements' => [
+                'method' => 'one',
+            ],
+        ],
         'method_argument_space'             => [
             'on_multiline'                     => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'indentation_type'                  => true,
-        'method_chaining_indentation'       => true,
+        'single_trait_insert_per_statement' => true,
     ])
     ->setFinder($finder);
