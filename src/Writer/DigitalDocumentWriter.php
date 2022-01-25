414f84057a759ecb8d5a4701e0e30f6ddfa536fb
itajackass <lodirizzini.giuseppe@gmail.com>
2022-01-25 08:25:17 +0100

    Aggiunto url in schemaLocation
    Senza tale url, le fatture mi venivano scartate dall' SDI

--- src/Writer/DigitalDocumentWriter.php
+++ src/Writer/DigitalDocumentWriter.php
@@ -140,7 +140,7 @@ class DigitalDocumentWriter implements DigitalDocumentWriterInterface
             'xmlns:xmlns:ds' => 'http://www.w3.org/2000/09/xmldsig#',
             'xmlns:xmlns:p' => 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2',
             'xmlns:xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-            'xsi:xsi:schemaLocation' => 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2',
+            'xsi:xsi:schemaLocation' => 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2 http://www.fatturapa.gov.it/export/fatturazione/sdi/fatturapa/v1.2/Schema_del_file_xml_FatturaPA_versione_1.2.xsd',
         ];
 
         foreach ($namespaces as $name => $value) {
